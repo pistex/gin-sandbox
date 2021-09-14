d417673b94581bd8bd130db84a1aa1bca6c3652e
Panithi Nakkhruea <panithi.nakkhruea@pm.me>
2021-09-14 19:11:29 +0700

    Change database schema migration tool to liquibase

--- server/database/migration/V1__create_user_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id uuid PRIMARY KEY,
-    email varchar(225) NOT NULL UNIQUE,
-    password varchar(255) NOT NULL,
-    created_at timestamptz NOT NULL DEFAULT (now()),
-    updated_at timestamptz NULL DEFAULT (null)
-);
